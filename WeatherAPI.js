4edb0c894e07fa09dc511ae47c4d42aa074352b9
Eddie Biggs <edbiggs12@gmail.com>
2023-09-06 09:37:06 -0400

    just tidying up a bit

--- WeatherAPI.js
+++ WeatherAPI.js
@@ -1,7 +1,7 @@
 const getWeather = async (e) => {
 
 
-    const location = document.getElementById('location')
+    const currentLocation = document.getElementById('current-location')
     // Thunderstorms; Drizzle; Rain; Snow; Atmosphere; Clear; Clouds;
     const weatherGroups = [[200,202,210,211,212,221,230,231,232],[300,301,302,310,311,312,313,,314,321],[500,501,502,503,504,511,520,521,522,531],[600,601,602,611,612,613,615,616,620,621,622],[701,711.721,731,741,751,761,762,771,781],[800],[801,802,803,804]]
     
@@ -14,19 +14,43 @@ const getWeather = async (e) => {
         navigator.geolocation.getCurrentPosition( async (position)=> {
             let lon = position.coords.longitude;
             let lat = position.coords.latitude;  
-            let url = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat  + "&lon=" + lon + "&appid=023a5325fe39bd32b15bc9c3e9cc66e9&units=imperial"
-            console.log(url)
-            const res = await fetch(url)
-            const data = await res.json()
-            console.log(url)
-            console.log(data)
 
-            const temperature = document.getElementById('temp')
-            const feelsLike = document.getElementById('feels-like')
-            const clouds = document.getElementById('clouds')
-            const humidity = document.getElementById('humidity')
+            let currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat  + "&lon=" + lon + "&appid=023a5325fe39bd32b15bc9c3e9cc66e9&units=imperial"
 
-            const weatherId = data.weather[0].id
+            // Forecast data disabled because it requires a paid subscription
+
+            // let ForecastUrl = "https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=" + lat + "&lon=" + lon + "&appid=" + "023a5325fe39bd32b15bc9c3e9cc66e9&units=imperial"
+ 
+
+            const resCurrent = await fetch(currentWeatherUrl)
+            const dataCurrent = await resCurrent.json()
+            // const resForecast = await fetch(ForecastUrl)
+            // const dataForecast = await resForecast.json()
+            // console.log(dataForecast)
+
+            const currentTemperature = document.getElementById('current-temp')
+            const currentFeelsLike = document.getElementById('current-feels-like')
+            const currentClouds = document.getElementById('current-clouds')
+            const currentHumidity = document.getElementById('current-humidity')
+
+            currentLocation.innerText = dataCurrent.name
+            currentTemperature.innerText = dataCurrent.main.temp + '°'
+            currentFeelsLike.innerText = "Feels like " + dataCurrent.main.feels_like + '°'
+            currentClouds.innerText = dataCurrent.weather[0].description
+            currentHumidity.innerText = "Humidity: " + dataCurrent.main.humidity + "%"
+
+            const forecastTemperature = document.getElementById('forecast-temp')
+            const forecastFeelsLike = document.getElementById('forecast-feels-like')
+            const forecastClouds = document.getElementById('forecast-clouds')
+            const forecastHumidity = document.getElementById('forecast-humidity')
+
+            // forecastLocation.innerText = dataForecast.name
+            // forecastTemperature.innerText = dataForecast.main.temp + '°'
+            // forecastFeelsLike.innerText = "Feels like " + dataForecast.main.feels_like + '°'
+            // forecastClouds.innerText = dataForecast.weather[0].description
+            // forecastHumidity.innerText = "Humidity: " + dataForecast.main.humidity + "%"
+
+            const weatherId = dataCurrent.weather[0].id
             console.log(weatherId)
             for (let i = 0; i < weatherGroups.length; i ++) {
                 console.log(weatherGroups[i])
@@ -41,11 +65,7 @@ const getWeather = async (e) => {
                 }
             
 
-            location.innerText = data.name
-            temperature.innerText = data.main.temp + '°'
-            feelsLike.innerText = "Feels like " + data.main.feels_like + '°'
-            clouds.innerText = data.weather[0].description
-            humidity.innerText = "Humidity: " + data.main.humidity + "%"
+
         },
         (error) => {
              console.log('Error: Geolocation not supported by this browser')
